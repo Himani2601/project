ff7ea6c620c0c44a92e850769c975eaef5e415fa
Rupesh Thakur <rupeshthakur80078@gmail.com>
2024-04-20 11:03:56 +0530

    Header stage-2

--- Client/src/components/Header/Header.jsx
+++ Client/src/components/Header/Header.jsx
@@ -1,35 +1,85 @@
 import React from 'react'
-import { Navbar, Button } from "flowbite-react"
+import { Navbar, Button, Dropdown, TextInput } from "flowbite-react";
+import { MdSearch, MdShoppingBasket } from "react-icons/md";
 import { NavLink, Link } from 'react-router-dom'
 
 const Header = () => {
+    const currentUser = false;
     return (
         <div className='fixed top-0 left-0 right-0 bg-white shadow-lg z-50'>
             <Navbar className='border-b-2'>
                 <Link to='#' className='self-center whitespace-nowrap text-sm sm:text-xl font-extrabold rounded-lg text-white' style={{ fontVariant: 'unicase' }}>
-                    <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%  rounded-lg inline-block text-transparent bg-clip-text'>Food Space</span>
+                    <span className='px-2 py-1 bg-gradient-to-r from-orange-500 from-30% via-sky-500 via-50% to-emerald-500 to-90%  rounded-lg inline-block text-transparent bg-clip-text'>Food Space</span>
 
                 </Link>
-                <div className='flex gap-3'>
-                    <NavLink
-                        to="#"
-                        className={({ isActive }) => `block py-2 duration-200 ${isActive ? "text-orange-700" : "text-gray-900"} font-semibold`}
-                    >
-                        Home
-                    </NavLink>
-                    <NavLink
-                        to="#"
-                        className={({ isActive }) => `block py-2 pr-[1vw] pl-[1vw] duration-200 ${isActive ? "text-orange-700" : "text-gray-900"} font-semibold`}
-                    // onClick={scrollToAbout}
-                    >
-                        About
-                    </NavLink>
-                    <Link to="#">
-                        <Button gradientDuoTone="purpleToPink" outline>
-                            Sign In
-                        </Button>
-                    </Link>
-                </div>
+                {currentUser ? (
+                    <>
+                        <div className='flex justify-center items-center md:pr-[5vw]'>
+                            <div className='hidden md:flex justify-center items-center lg:gap-5 md:gap-0'>
+                                <NavLink
+                                    to="#"
+                                    className={({ isActive }) => `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-black" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent md:border-0 hover:text-teal-600 lg:p-0 font-semibold`}
+                                >
+                                    Menu
+                                </NavLink>
+                                <TextInput
+                                    type="text"
+                                    placeholder="Search Items.."
+                                    icon={MdSearch}
+                                    className="border-b w-[100%] my-2"
+                                    style={{ height: "5vh", outline: "none" }}
+                                />
+                                <NavLink
+                                    to="#"
+                                    className={({ isActive }) => `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-black" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent md:border-0 hover:text-teal-600 lg:p-0 font-semibold`}
+                                >
+
+                                    <MdShoppingBasket className='w-7 h-7' />
+                                </NavLink>
+                            </div>
+                        </div>
+                        <div className='flex gap-2'>
+                            <Dropdown
+                                inline
+                            >
+                                <Dropdown.Header>
+                                </Dropdown.Header>
+                                Logged User
+                                <Dropdown.Divider />
+                                <Dropdown.Item className='text-md'
+                                // onClick={handleSignout}
+                                >Sign Out</Dropdown.Item>
+                            </Dropdown>
+                        </div>
+                    </>
+                ) : (
+                    <div className='flex gap-3'>
+                        <NavLink
+                            to="#"
+                            className={({ isActive }) => `block py-2 duration-200 ${isActive ? "text-orange-700" : "text-gray-900"} font-semibold`}
+                        >
+                            Home
+                        </NavLink>
+                        <NavLink
+                            to="#"
+                            className={({ isActive }) => `block py-2 pr-[1vw] pl-[1vw] duration-200 ${isActive ? "text-orange-700" : "text-gray-900"} font-semibold`}
+                        // onClick={scrollToAbout}
+                        >
+                            About
+                        </NavLink>
+                        <NavLink
+                            to="#"
+                            className={({ isActive }) => `block py-2 duration-200 ${isActive ? "text-orange-700" : "text-gray-900"} font-semibold mr-3`}
+                        >
+                            Contact Us
+                        </NavLink>
+                        <Link to="#">
+                            <Button gradientDuoTone="purpleToPink" outline>
+                                Sign In
+                            </Button>
+                        </Link>
+                    </div>
+                )}
             </Navbar >
         </div>
     )
